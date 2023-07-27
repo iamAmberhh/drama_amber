9c7f7ac40ebd256da235d1c3a3334536723d4fa2
iamAmberhh <melody19941231@gmail.com>
2023-07-27 19:25:01 +0800

    first init

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container">
+    <div class="phone">
+      <span class="phone-direction phone-direction-1"></span>
+      <span class="phone-direction phone-direction-2"></span>
+      <div class="top-block"></div>
+      <Enter />
+      <div class="footer-name">@drama_</div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import Enter from '@/components/Enter.vue'
+</script>
+
+<style lang="scss" scoped>
+@import './assets/all.scss';
+</style>
---
+++ src/components/Enter.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="time"></div>
+  <div class="message-box">
+    <div class="box-title">
+      <div>
+        <img src="" alt="img" />
+        <span class="message">MESSAGE</span>
+      </div>
+      <span class="now">NOW</span>
+    </div>
+    <div class="content">hello</div>
+  </div>
+  <div class="search-bar">
+    Let's start !
+    <i class="bi bi-search"></i>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+defineProps({
+  msg: String,
+});
+
+</script>
+
+<style scoped lang="scss">
+
+$brown: #7F664F;
+.time {
+  font-weight: bold;
+  font-size: 50px;
+  color: $brown;
+  text-align: center;
+  margin: 10px;
+  height: 80px;
+}
+
+.message-box {
+  border-radius: 15px;
+  background-color: rgba(204, 213, 174, 0.6);
+  width: 90%;
+  height: 250px;
+  margin: 0 auto;
+}
+
+.box-title {
+  padding: 15px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border-bottom: 2px solid white;
+
+  img {
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+    object-fit: cover;
+    margin-right: 5px;
+  }
+
+  .message {
+    font-weight: bold;
+  }
+
+  .now {
+    font-size: 12px;
+  }
+}
+
+
+</style>
